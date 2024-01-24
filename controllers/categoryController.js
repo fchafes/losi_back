a883dc1067ba4d78d5ef682896128cb03ccd4e28
ThomiHughes <thomih44@gmail.com>
2024-01-24 14:27:23 -0300

    Update categoryController.js

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,31 +1,24 @@
 const { Product , Category} = require("../models");
 
+
 async function category(req, res) {
     try {
-      const categoryName = req.params.categoryName;
-  
-      // Find the Category by name to get its id
-      const category = await Category.findOne({
-        where: { name: categoryName },
-      });
-  
-      if (!category) {
-        res.status(404).send('Category not found');
-        return;
-      }
-  
-      // Fetch products with the found categoryId and include the associated Category
-      const products = await Product.findAll({
-        where: { categoryId: category.id },
-        include: Category,
-      });
-  
-      res.json(products);
+        const categoryName = req.params.categoryName;
+
+        // Fetch products with the specified categoryName and include the associated Category
+        const products = await Product.findAll({
+            include: {
+                model: Category,
+                where: { name: categoryName },
+            },
+        });
+
+        res.json(products);
     } catch (error) {
-      console.error('Error fetching products:', error);
-      res.status(500).send('An error occurred.');
+        console.error("Error fetching products:", error);
+        res.status(500).send("An error occurred.");
     }
-  }
+}
 
   module.exports = {
     category
