1d8d7ff35e4762394fa4a5b7394d417e198daf41
juaniboni <juanignacioboni@gmail.com>
2024-02-01 22:15:50 -0300

    Customer Controller

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -1,5 +1,19 @@
+const { Customer } = require("../models");
 // Display a listing of the resource.
-async function index(req, res) {}
+async function index(req, res) {
+  try {
+    const customers = await Customer.findAll();
+
+    if (customers.length === 0) {
+      return res.status(404).json({ message: 'No se encontraron clientes.' });
+    }
+
+    res.status(200).json(customers);
+  } catch (error) {
+    console.error('Error al obtener clientes:', error);
+    res.status(500).json({ error: 'Error interno del servidor' });
+  }
+}
 
 // Display the specified resource.
 async function show(req, res) {}
