5c18f581e1639c015af2f733a49d92e68f322062
fchafes <64428168+fchafes@users.noreply.github.com>
2024-02-13 11:07:29 -0300

    rutas privadas

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const categoryController = require("../controllers/categoryController");
+const { expressjwt: checkJwt } = require("express-jwt");
 
 // Rutas relacionadas a los usuarios:
 // ...
 
 router.get("/", categoryController.index);
-// router.get("/featured", categoryController.featured);
-router.post("/", categoryController.store);
 router.get("/:categoryName", categoryController.category);
+
+router.use(checkJwt({ secret: "stringsecreto", algorithms: ["HS256"] }));
+
+router.post("/", categoryController.store);
 router.patch("/:id", categoryController.update);
 router.delete("/:id", categoryController.destroy);
 
