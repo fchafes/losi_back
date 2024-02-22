052da17c7fec857b188a0972beb0fbe856929615
juaniboni <juanignacioboni@gmail.com>
2024-02-21 22:14:14 -0300

    cambios mercado pago

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,27 +1,34 @@
-const { MercadoPago } = require('mercadopago');
+const {MercadoPagoConfig,Preference} = require('mercadopago');
+// Agrega credenciales
+const client = new MercadoPagoConfig({ accessToken: 'TEST-2509782001690764-022117-bedefb8cee5cd87c46026a7a9b95368b-274998896' });
 
 // Store a newly created resource in storage.
 async function store(req, res) {
   try {
-    const preference = {
+    const preferenceData = {
       items: [
         {
-          title: req.body.title,
-          quantity: Number(req.body.quantity),
-          unit_price: Number(req.body.price),
-          currency_id: "UYS"
+          productId: req.body.cartItems.productId,
+          quantity: Number(req.body.cartItems.quantity),
+          price: Number(req.body.total),
+          unit_price: Number(req.body.cartItems.unit_price),
+          currency_id: "UYU",
+          name: req.body.cartItems.name,
         }
+      
       ],
       back_urls: {
-        success: "https://www.youtube.com/watch?v=-VD-l5BQsuE&ab_channel=onthecode",
-        failure: "https://www.youtube.com/watch?v=-VD-l5BQsuE&ab_channel=onthecode",
-        pending: "https://www.youtube.com/watch?v=-VD-l5BQsuE&ab_channel=onthecode"
+        success: "http://localhost:5173/",
+        failure: "http://localhost:5173/",
+        pending: "http://localhost:5173/"
       },
       auto_return: "approved"
     };
-
-    const response = await MercadoPago.preferences.create(preference);
-    res.json({ id: response.body.id });
+    console.log(req.body);
+    console.log("hola");
+    const preference= new Preference(client);
+    const response = await preference.create({body:preferenceData});
+    res.json(response);
   } catch (error) {
     console.error("Error creating preference:", error);
     res.status(500).json({ error: "Internal Server Error" });
--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const paymentController = require("../controllers/adminController");
+const paymentController = require("../controllers/paymentController");
 
 // Rutas relacionadas a los usuarios:
 // ...
